4859382890f93130a06c5f1dafa1343be2321afb
bige-ye <819423475@qq.com>
2017-12-01 17:30:49 +0800

    去重

--- app/src/main/java/com/yechaoa/printertest/BluetoothBean.java
+++ app/src/main/java/com/yechaoa/printertest/BluetoothBean.java
@@ -8,8 +8,25 @@ import android.bluetooth.BluetoothDevice;
  */
 
 public class BluetoothBean {
+
+    public BluetoothBean(){}
+
     public int mBluetoothId;
     public String mBluetoothName;//蓝牙名字
     public String mBluetoothAddress;//蓝牙地址
     public BluetoothDevice mBluetoothDevice;//蓝牙设备
+
+    @Override
+    public boolean equals(Object obj) {
+        BluetoothBean b=(BluetoothBean)obj;
+        return mBluetoothAddress.equals(b.mBluetoothAddress) ;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = mBluetoothAddress.hashCode();
+        result = 31 * result + mBluetoothAddress.hashCode();
+        return result;
+    }
+
 }
